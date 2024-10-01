dbbb74ef2feef735dd9c8049dd42164de98419d9
Muhammad Inam <nudmaufon@gmail.com>
2024-10-01 18:46:51 +0500

    textures_loader

--- 02-ThreeJS/Meshes/meshBasicStanderd.js
+++ 02-ThreeJS/Meshes/meshBasicStanderd.js
@@ -1,3 +1,4 @@
+import * as lil from "lil-gui";
 import * as Three from "three";
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
 
@@ -27,13 +28,20 @@ function addLightHelper(light) {
     const helper = new Three.PointLightHelper(light);
     scene.add(helper);
 }
-
 addLightHelper(pointLight);
 addLightHelper(directionalLight);
 
+const loader = new Three.TextureLoader();
+const color = loader.load("../text/color.jpg")
+const roughness = loader.load("../text/roughness.jpg")
+const normal = loader.load("../text/normal.png")
+const height = loader.load("../text/height.png")
+const emisive = loader.load("../text/emissive.jpg")
+
+
 //* geometry name and material
-const geometry = new Three.BoxGeometry(1, 1, 1);
-const material = new Three.MeshStandardMaterial({color: "red" ,roughness: 0, metalness: 0});
+const geometry = new Three.BoxGeometry(2, 1,2);
+const material = new Three.MeshStandardMaterial({map: color ,roughnessMap: roughness, normalMap: normal, displacementMap: height, displacementScale: 0, emisiveMap: emisive});
 const cube = new Three.Mesh(geometry, material);
 scene.add(cube);
 camera.position.z = 5;
@@ -45,6 +53,22 @@ renderer.setSize(window.innerWidth, window.innerHeight);
 //* orbitControls seetings
 const control = new OrbitControls(camera, renderer.domElement);
 control.enableDamping = true;
+
+const gui = new lil.GUI();
+const materialFolder = gui.addFolder("Material");
+materialFolder.add(material, "roughness", 0, 1).name("Roughness");
+materialFolder.add(material, "metalness", 0, 1).name("Metalness");
+materialFolder.add(material, "transparent", false).name("Transparent");
+materialFolder.add(material, "opacity", 0, 1).name("Opacity");
+materialFolder.add(material, "displacementScale", 0, 1).name("Displacement Scale");
+materialFolder.add(material, "displacementBias", -1, 1).name("Displacement Bias");
+materialFolder.open();
+
+const meshFolder = gui.addFolder("Mesh");
+meshFolder.add(cube.scale, "x", 0, 5).name("Scale X");
+meshFolder.add(cube.scale, "y", 0, 5).name("Scale Y");
+meshFolder.add(cube.scale, "z", 0, 5).name("Scale Z");
+meshFolder.open();
 
 
 //* animation of cube
